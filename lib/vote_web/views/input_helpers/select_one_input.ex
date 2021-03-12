7dd3a602a6e8fb55c468b4ccf8f0c8e12fe22e01
Michael Eden <themichaeleden@gmail.com>
2021-03-12 11:27:30 -0500

    add plurality voting method form helper

---
+++ lib/vote_web/views/input_helpers/select_one_input.ex
@@ -0,0 +1,54 @@
+defmodule VoteWeb.Views.InputHelpers.SelectOneInput do
+  use Phoenix.HTML
+  alias Phoenix.HTML.Form
+
+  def select_one_input(form, field, options, params \\ []) do
+    table_opts = [
+      id: Form.input_id(form, field),
+      class: "select-one-table",
+    ]
+    content_tag(:table, table_opts) do
+      [
+        table_header(),
+        table_body(form, field, options, params),
+      ]
+    end
+  end
+
+  defp table_header() do
+    content_tag(:thead) do
+      content_tag(:tr) do
+        [
+          content_tag(:th, "Option", class: "opt-name-header"),
+          content_tag(:th, "Select:", class: "opt-select-one-header"),
+        ]
+      end
+    end
+  end
+
+  defp table_body(form, field, options, params) do
+    options
+    |> Enum.with_index()
+    |> Enum.map(fn {o, i} -> table_row(form, field, o, i, params) end)
+  end
+
+  defp table_row(form, field, opt, opt_idx, params) do
+    id = Form.input_id(form, field)
+    name = Form.input_name(form, field)
+    value = Form.input_value(form, field)
+
+    input_opts = [
+      id: "#{id}_#{opt_idx}",
+      name: "#{name}",
+    ] ++ (if value[opt_idx] do [checked: true] else [] end)
+
+    content_tag(:tr) do
+      [
+        content_tag(:td, opt, class: "opt-name"),
+        content_tag(:td, class: "opt-select-one") do
+          Form.radio_button(form, :nothing, opt_idx, input_opts)
+        end,
+      ]
+    end
+  end
+end
